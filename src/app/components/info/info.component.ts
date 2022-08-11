b8574b6bf3ede75011719e46325a0e0ed41c30d3
Guadalupe Evequoz <guadalupe.evequoz@gmail.com>
2022-08-11 20:12:09 -0300

    cambios para q sea más responsive

--- src/app/components/info/info.component.ts
+++ src/app/components/info/info.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of, skip } from 'rxjs';
 import { Informacion } from 'src/app/models/informacion';
 
 @Component({
@@ -8,32 +7,10 @@ import { Informacion } from 'src/app/models/informacion';
   styleUrls: ['./info.component.css'],
 })
 export class InfoComponent implements OnInit {
-  titulo: string = '';
+  titulo: string = '<Guadalupe Evequoz />';
   data: Informacion = new Informacion(0, '', '', '');
 
   constructor() {}
 
-  ngOnInit(): void {
-    this.write();
-  }
-
-  onClick(): void {
-    this.write();
-  }
-
-  private write(): void {
-    let msg: string = '<Guadalupe Evequoz />';
-    this.titulo = '_';
-
-    for (let i = 0; i < msg.length; i++) {
-      setTimeout(() => {
-        const chars = [...this.titulo];
-        chars[i] = msg[i];
-        if (i < msg.length - 1) {
-          chars[i + 1] = '_';
-        }
-        this.titulo = chars.join('');
-      }, 100 * i);
-    }
-  }
+  ngOnInit(): void {}
 }
